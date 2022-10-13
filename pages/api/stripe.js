3cae253a74d4ad9f6088cd672d43cc4ddaa70b2d
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2022-10-14 00:12:49 +0300

    Ecommerce

---
+++ pages/api/stripe.js
@@ -0,0 +1,50 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe("sk_test_51LsXK7FqHewPzjUWQUqc5Z2L9wsf7xFAMAMyTv1oMleSswQAMPCoQlrA0AkeVu6PTt9Cqm8DjissZ3d1JXn7s1hz00u4oHJY81");
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+          { shipping_rate: 'shr_1LsXSjFqHewPzjUWyB0R5ksS' },
+        ],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/rmgcp6tl/production/').replace('-webp', '.webp');
+
+          return {
+            price_data: { 
+              currency: 'try',
+              product_data: { 
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled:true,
+              minimum: 1,
+            },
+            quantity: item.quantity
+          }
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
+      }
+
+      const session = await stripe.checkout.sessions.create(params);
+
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
